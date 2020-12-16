b409323687099bb5384d2d72de3aae955ef3c187
Jakob Danilo <danilo.jakob@wuerth-itensis.com>
2020-12-16 08:43:33 +0100

    CHANGE: Get weather from Chur and log data

---
+++ src/app/service/api.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  private readonly API_KEY = '10d503f6b26b8edaa17f7c5a58d8e73e';
+
+  constructor(private http: HttpClient) { }
+
+  public getTestData(): Observable<object> {
+    return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=Chur&appid=${this.API_KEY}`);
+  }
+}
