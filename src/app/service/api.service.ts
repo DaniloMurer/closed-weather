562d28bee362509c2c8d6f300be54523c439e99d
Jakob Danilo <danilo.jakob@wuerth-itensis.com>
2020-12-22 08:57:41 +0100

    CHANGE: Fixed TS-Lint errors

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Observable} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
-import {Weather} from "../../data/weather";
+import {Weather} from '../../data/weather';
 
 @Injectable({
   providedIn: 'root'
